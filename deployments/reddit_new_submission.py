eaa17e7e543bd1b468abfcc6bb63ce4e6d0c41a1
ramos <crqdev@gmail.com>
2023-03-15 22:37:39 -0400

    update queue name and schedule

--- deployments/reddit_new_submission.py
+++ deployments/reddit_new_submission.py
@@ -1,6 +1,7 @@
 from flows.etl.reddit.new_submissions_flow import new_submissions_flow
 from prefect.deployments import Deployment
 from prefect.blocks.core import Block
+from prefect.server.schemas.schedules import IntervalSchedule
 
 
 storage = Block.load("github/veloslab-prefect")
@@ -9,8 +10,10 @@ buildapcsales = Deployment.build_from_flow(
     flow=new_submissions_flow,
     name="buildapcsales",
     work_pool_name='veloslab',
+    work_queue_name='reddit_new_submissions',
     parameters={"subreddit": "buildapcsales"},
     storage=storage,
+    schedule=(IntervalSchedule(interval=60)),
 )
 
 if __name__ == "__main__":
