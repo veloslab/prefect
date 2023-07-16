15fabe108e7279ca47cb1d8827a51824255e2599
Carlos Ramos <crqdev@gmail.com>
2023-07-15 22:40:42 -0400

    initial add

--- utility/server/transfer.py
+++ utility/server/transfer.py
@@ -1,4 +1,5 @@
 from utility.server.util import ssh_connection
+from typing import List
 
 
 def scp(source: str, remote: str, ssh_secret: str) -> None:
@@ -12,3 +13,14 @@ def scp(source: str, remote: str, ssh_secret: str) -> None:
     with ssh_connection(ssh_secret) as conn:
         conn.put(source, remote)
 
+
+def remote_ls(path: str, ssh_secret: str) -> List:
+    """
+    List files on remote server
+    :param path: Directory path on remote host
+    :param ssh_secret: Vault secret to SSH connection details such as host, user, private_key
+    :return:
+    """
+    with ssh_connection(ssh_secret) as ssh_conn:
+        result = ssh_conn.run(f"ls {path}", hide=True)
+    return result.stdout.splitlines()
