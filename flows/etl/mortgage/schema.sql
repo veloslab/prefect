4d739056de928daaceb45bec3738023255a34e16
ramos <crqdev@gmail.com>
2024-09-22 23:09:45 -0400

    add slack notification

--- flows/etl/mortgage/schema.sql
+++ flows/etl/mortgage/schema.sql
@@ -1,8 +1,16 @@
-CREATE TABLE `prefect`.`mortgage_rates` (
+CREATE TABLE `prefect`.`mortgage_rates_history` (
   `posted` DATETIME NOT NULL,
   `bank` varchar(20) NOT NULL,
   `term` varchar(50) NOT NULL,
   `rate` decimal(6,3) DEFAULT NULL,
   `discount_points` decimal(6,3) DEFAULT NULL,
   PRIMARY KEY (`posted`, `bank`, `term`)
+);
+CREATE TABLE `prefect`.`mortgage_rates_latest` (
+  `bank` varchar(20) NOT NULL,
+  `term` varchar(50) NOT NULL,
+  `rate` decimal(6,3) DEFAULT NULL,
+  `discount_points` decimal(6,3) DEFAULT NULL,
+  `posted` DATETIME NOT NULL,
+  PRIMARY KEY (`bank`, `term`)
 );
