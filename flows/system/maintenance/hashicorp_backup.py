c00afe8be0e9a3a39642430b4f1ead50a518529a
ramos <crqdev@gmail.com>
2024-10-02 22:35:03 +0000

    add b2 lib and fix vault

--- flows/system/maintenance/hashicorp_backup.py
+++ flows/system/maintenance/hashicorp_backup.py
@@ -1,11 +1,11 @@
 from prefect import flow, get_run_logger
-from prefect.task_runners import SequentialTaskRunner
 from datetime import datetime
 from utility.hashicorp import Vault
 from utility.server import scp, tmp_file
+from utility.backblaze import Bucket
 
 
-@flow(task_runner=SequentialTaskRunner())
+@flow()
 def hashicorp_vault_backup_flow():
     logger = get_run_logger()
     logger.info("Initiated Hashicorp Vault Raft Backup")
@@ -14,10 +14,10 @@ def hashicorp_vault_backup_flow():
     logger.info("Retrieved Hashicorp Vault Raft Backup")
     with tmp_file(response.content, prefix='hc_vault') as file:
         logger.info(f"Persisted Response to localhost: {file}")
-        scp(file,
-            f"/mnt/storage-ssd/prefect/hashicorp/vault/{dt.strftime('%Y-%m-%dT%H%M%S')}.backup.snap",
-            'ssh/truenas/prefect')
-        logger.info(f"Transferred Backup to Truenas: {dt.strftime('%Y-%m-%dT%H%M%S')}.backup.snap")
+        filename = f"{dt.strftime('%Y-%m-%dT%H%M%S')}.vls.snap"
+        bucket = Bucket('prefect-vault')
+        bucket.upload_file(file, filename)
+        logger.info(f"Transferred {filename} to Vault Bucket")
 
 
 if __name__ == "__main__":
